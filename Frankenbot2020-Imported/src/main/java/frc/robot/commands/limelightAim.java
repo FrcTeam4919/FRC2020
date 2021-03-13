8eacd7d201143c3cbeb99b92bc6bec4b8f11729b
HunterPaul4919 <47090143+HunterPaul4919@users.noreply.github.com>
2021-03-13 15:44:45 -0800

    Imported to 2021

---
+++ Frankenbot2020-Imported/src/main/java/frc/robot/commands/limelightAim.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.*;
+
+public class limelightAim extends Command {
+    static double kp = -0.1f;
+    double min_command = 0.05f;
+    double tx = Limelight.tx.getDouble(0.0);
+    double y = OI.joystick.getY();
+    double x = OI.joystick.getX();
+
+    public limelightAim() {
+        
+    }
+    protected void initialize(){
+        System.out.println("Aiming");
+    }
+
+    protected void execute(){
+        double heading_error = -tx;
+        double steering_adjust = 0.0f;
+        if(tx > 1.0){
+            steering_adjust = kp*heading_error - min_command;
+        } else if(tx < 1.0){
+            steering_adjust = kp*heading_error + min_command;
+        }
+        Drivetrain.westCoastTankDrive.arcadeDrive(x+=steering_adjust, y-=steering_adjust);
+    }
+
+    protected boolean isFinished(){
+        return false;
+    }
+}
